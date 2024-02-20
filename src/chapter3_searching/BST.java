0f028ca4beb3ed1fbd8a3db2d53d1a2cc7c05ad8
chengwen-zheng <chengwenzheng1222@gmail.com>
2024-02-20 14:32:23 +0800

    feat: add isBST for BST

--- src/chapter3_searching/BST.java
+++ src/chapter3_searching/BST.java
@@ -278,11 +278,29 @@ public class BST<Key extends Comparable<Key>, Value> {
         return height(root);
     }
 
-    public int height(Node x) {
+    private int height(Node x) {
         if (x == null) {
             return -1;
         }
         return 1 + Math.max(height(x.left), height(x.right));
+    }
+
+    // Exercise 3.2.32
+    public boolean isBST() {
+        return isBST(root, null, null);
+    }
+
+    private boolean isBST(Node x, Key min, Key max){
+        if (x == null) {
+            return true;
+        }
+        if (min != null && x.key.compareTo(min) <= 0) {
+            return false;
+        }
+        if (max != null && x.key.compareTo(max) >= 0) {
+            return false;
+        }
+        return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
     }
 
 
